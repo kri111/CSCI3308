448e73c3e34826a19ca0794902cd6725e5019338
kri111 <krishna.adettiwar@colorado.edu>
2017-09-22 10:47:42 -0600

    adding other files

---
+++ test.c
@@ -0,0 +1,11 @@
+       #define _GNU_SOURCE
+       #include <unistd.h>
+       #include <sys/syscall.h>
+       #include <sys/types.h>
+       #include <signal.h>
+
+int main(int argc, char *argv[])
+       {
+           int x = syscall(332);
+
+	}
